298f9d1d073eb072a2002692f335f9d7bd5e7671
danieleongari <daniele.ongari@gmail.com>
2024-10-13 08:13:51 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# voicemydocs
+
+Convert PDF files to engeging audios, with full control over the whole process.
+
+
+## Development
+
+Ruff linting and formatting.
+
+```bash
+pip install -e.[dev]
+pre-commit install
+```
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='voicemydocs',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'dash',
+        'dash-bootstrap-components',
+    ],
+    extras_require={
+        'dev': [
+            'pre-commit',
+            'ruff=0.6.9',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'voicemydocs=voicemydocs.main:main',
+        ],
+    },
+)
